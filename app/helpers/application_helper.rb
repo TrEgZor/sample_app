6d963ec3ad56a3ae5c728af4a7de5446c1b91225
tregzor <lmdavidborka@gmail.com>
2017-04-13 03:06:38 +0200

    Add a full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def fullTitle (page_title = ' ')
+    baseTitle = "Rails Sample App"
+    if page_title.empty?
+      baseTitle
+    else
+      page_title +" | "+baseTitle
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,33 +2,33 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
-    @baseTitle = " | Rails Sample App"
+    @baseTitle = "Rails Sample App"
   end
   test "should get root" do
     get static_pages_home_url
-    assert_select "title", "Home#{@baseTitle}"
+    assert_select "title", "#{@baseTitle}"
     assert_response :success
   end
   test "should get home" do
     get static_pages_home_url
-    assert_select "title", "Home#{@baseTitle}"
+    assert_select "title", "#{@baseTitle}"
     assert_response :success
   end
 
   test "should get help" do
     get static_pages_help_url
-    assert_select "title", "Help#{@baseTitle}"
+    assert_select "title", "Help | #{@baseTitle}"
     assert_response :success
   end
   test "should get about" do
     get static_pages_about_url
-    assert_select "title", "About#{@baseTitle}"
+    assert_select "title", "About | #{@baseTitle}"
     assert_response :success
   end
 
   test "should get contact" do
     get static_pages_contact_url
-    assert_select "title" , "Contact#{@baseTitle}"
+    assert_select "title" , "Contact | #{@baseTitle}"
     assert_response :success
   end
 end
